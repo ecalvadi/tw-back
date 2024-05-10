cc76b8820045328a0af8c3e09363afb4753a3cb6
Héctor Álvarez Díaz <hector@eclipsedpixel.com>
2024-05-10 09:23:12 -0400

    JWT ok

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,13 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        User::create([
+            'name' => 'Demo User',
+            'email' => 'test@demo.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('demo123'), // password
+            'remember_token' => Str::random(10),
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
